49b193505312f93b112052bd1dfc43b604b78d78
MoeThun <dhtmitmi@gmail.com>
2024-03-15 11:55:20 +0800

    feat: add willowbloosm icon

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -12,9 +12,16 @@ use bevy::{
 use bevy_egui::{
     egui::{
         self,
+        epaint::CircleShape,
         ColorImage,
+        Painter,
+        Response,
+        Sense,
+        Stroke,
         TextureHandle,
         TextureOptions,
+        Vec2,
+        Widget,
     },
     EguiContexts,
     EguiPlugin,
@@ -50,6 +57,35 @@ pub struct GIFImages {
     images: HashMap<String, Vec<(TextureHandle, u32)>>,
 }
 
+pub struct CircleImageButton {
+    image: egui::TextureId,
+    size: f32,
+}
+
+impl CircleImageButton {
+    pub fn new(image: egui::TextureId, size: f32) -> Self { Self { image, size } }
+}
+
+impl Widget for CircleImageButton {
+    fn ui(self, ui: &mut egui::Ui) -> Response {
+        let (rect, response) = ui.allocate_exact_size(Vec2::splat(self.size), Sense::click());
+        let painter = Painter::new(ui.ctx().clone(), ui.layer_id(), rect);
+        painter.add(egui::Shape::Circle(CircleShape {
+            center: rect.center(),
+            radius: self.size / 2.0,
+            fill: Default::default(),
+            stroke: Stroke::NONE,
+        }));
+        painter.add(egui::Shape::image(
+            self.image,
+            bevy_egui::egui::Rect::from_center_size(rect.center(), Vec2::splat(self.size)),
+            bevy_egui::egui::Rect::from_center_size(rect.center(), Vec2::splat(self.size)),
+            egui::Color32::WHITE,
+        ));
+        response
+    }
+}
+
 impl Plugin for UIPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(EguiPlugin)
@@ -68,9 +104,11 @@ pub fn setup_system(
     mut egui_context: EguiContexts,
     mut windows: Query<&mut Window>,
 ) {
+    let ctx = egui_context.ctx_mut();
     let mut window = windows.single_mut();
     window.ime_enabled = true;
     let mut txt_font = egui::FontDefinitions::default();
+    egui_extras::install_image_loaders(&ctx);
     txt_font
         .families
         .get_mut(&egui::FontFamily::Proportional)
@@ -96,10 +134,9 @@ pub fn ui_system(
     mut gif_images: ResMut<GIFImages>,
 ) {
     let ctx = contexts.ctx_mut();
-    let mut total_rows = 0;
     let target_message_key = "1670426821";
     let mut heights: Vec<f32> = vec![];
-
+    let willowbloosm_icon = egui::include_image!("../../assets/icons/willowbloosm.jpg");
     for message in manager.messages.get_mut(target_message_key).unwrap() {
         total_rows += 1;
         let mut height: f32 = 32.0;
@@ -119,99 +156,109 @@ pub fn ui_system(
         heights.push(height)
     }
 
-    egui_extras::install_image_loaders(&ctx);
-    egui::TopBottomPanel::top("top_panel")
-        .resizable(true)
+    egui::SidePanel::left("party_panel")
+        .exact_width(64.0)
         .show(ctx, |ui| {
-            ui.label("Top resizeable panel");
-            ui.allocate_rect(
-                ui.available_rect_before_wrap(),
-                egui::Sense::hover(),
-            );
-        });
-    egui::SidePanel::left("left_panel")
-        .resizable(true)
-        .show(ctx, |ui| {
-            ui.label("Left resizeable panel");
-            ui.allocate_rect(
-                ui.available_rect_before_wrap(),
-                egui::Sense::hover(),
-            );
-        });
-    egui::SidePanel::right("right_panel")
-        .resizable(true)
-        .show(ctx, |ui| {
-            ui.label("Right resizeable panel");
-            ui.allocate_rect(
-                ui.available_rect_before_wrap(),
-                egui::Sense::hover(),
-            );
+            ui.add_sized(
+                Vec2 { x: 64.0, y: 64.0 },
+                egui::Button::image(willowbloosm_icon),
+            )
         });
     egui::CentralPanel::default().show(ctx, |ui| {
-        egui::TopBottomPanel::bottom("input_panel")
+        egui::TopBottomPanel::top("top_panel")
             .resizable(true)
             .show(ctx, |ui| {
-                ui.with_layout(
-                    egui::Layout::bottom_up(egui::Align::Center),
-                    |ui| {
-                        let _teo_m = ime.text_edit_multiline(
-                            &mut app.multi_text,
-                            ui.max_rect().width(),
-                            ui,
-                            ctx,
-                            sender.as_ref(),
-                            &mut manager,
-                        );
-                    },
-                )
+                ui.label("Top resizeable panel");
+                ui.allocate_rect(
+                    ui.available_rect_before_wrap(),
+                    egui::Sense::hover(),
+                );
+            });
+        egui::SidePanel::left("left_panel")
+            .resizable(true)
+            .show(ctx, |ui| {
+                ui.label("Left resizeable panel");
+                ui.allocate_rect(
+                    ui.available_rect_before_wrap(),
+                    egui::Sense::hover(),
+                );
+            });
+        egui::SidePanel::right("right_panel")
+            .resizable(true)
+            .show(ctx, |ui| {
+                ui.label("Right resizeable panel");
+                ui.allocate_rect(
+                    ui.available_rect_before_wrap(),
+                    egui::Sense::hover(),
+                );
             });
         egui::CentralPanel::default().show(ctx, |ui| {
-            TableBuilder::new(ui)
-                .striped(true)
-                .resizable(false)
-                .auto_shrink(false)
-                .cell_layout(egui::Layout::top_down(
-                    egui::Align::LEFT,
-                ))
-                .stick_to_bottom(true)
-                .column(Column::remainder())
-                .min_scrolled_height(0.0)
-                .body(|body| {
-                    body.heterogeneous_rows(heights.into_iter(), |mut row| {
-                        let row_index = row.index();
-                        let message =
-                            &manager.messages.get_mut(target_message_key).unwrap()[row_index];
-                        row.col(|ui| {
-                            ui.label(message.data.sender.nickname.to_owned());
-                            for chain in &message.data.message_chain {
-                                match &chain.variant {
-                                    MiraiMessageChainType::Plain(plain) => {
-                                        let text = format!("{}", plain.text);
-                                        ui.add(egui::Label::new(text));
-                                    },
-                                    MiraiMessageChainType::Source(_) => {},
-                                    MiraiMessageChainType::Image(image) => {
-                                        if image.image_type == "GIF" {
-                                            if !gif_images.images.contains_key(&image.image_id) {
-                                                let img_bytes =
-                                                    reqwest::blocking::get(image.url.to_owned())
-                                                        .unwrap()
-                                                        .bytes()
-                                                        .unwrap();
-                                                let cursor = Cursor::new(img_bytes);
-                                                let decoder = GifDecoder::new(cursor).unwrap();
+            egui::TopBottomPanel::bottom("input_panel")
+                .resizable(true)
+                .show(ctx, |ui| {
+                    ui.with_layout(
+                        egui::Layout::bottom_up(egui::Align::Center),
+                        |ui| {
+                            let _teo_m = ime.text_edit_multiline(
+                                &mut app.multi_text,
+                                ui.max_rect().width(),
+                                ui,
+                                ctx,
+                                sender.as_ref(),
+                                &mut manager,
+                            );
+                        },
+                    )
+                });
+            egui::CentralPanel::default().show(ctx, |ui| {
+                TableBuilder::new(ui)
+                    .striped(true)
+                    .resizable(false)
+                    .auto_shrink(false)
+                    .cell_layout(egui::Layout::top_down(
+                        egui::Align::LEFT,
+                    ))
+                    .stick_to_bottom(true)
+                    .column(Column::remainder())
+                    .min_scrolled_height(0.0)
+                    .body(|body| {
+                        body.heterogeneous_rows(heights.into_iter(), |mut row| {
+                            let row_index = row.index();
+                            let message =
+                                &manager.messages.get_mut(target_message_key).unwrap()[row_index];
+                            row.col(|ui| {
+                                ui.label(message.data.sender.nickname.to_owned());
+                                for chain in &message.data.message_chain {
+                                    match &chain.variant {
+                                        MiraiMessageChainType::Plain(plain) => {
+                                            let text = format!("{}", plain.text);
+                                            ui.add(egui::Label::new(text));
+                                        },
+                                        MiraiMessageChainType::Source(_) => {},
+                                        MiraiMessageChainType::Image(image) => {
+                                            if image.image_type == "GIF" {
+                                                if !gif_images.images.contains_key(&image.image_id)
+                                                {
+                                                    let img_bytes = reqwest::blocking::get(
+                                                        image.url.to_owned(),
+                                                    )
+                                                    .unwrap()
+                                                    .bytes()
+                                                    .unwrap();
+                                                    let cursor = Cursor::new(img_bytes);
+                                                    let decoder = GifDecoder::new(cursor).unwrap();
 
-                                                let frames = decoder
-                                                    .into_frames()
-                                                    .collect_frames()
-                                                    .expect("Can't decode frames");
-                                                gif_images.images.insert(
-                                                    image.image_id.to_owned(),
-                                                    frames
-                                                        .iter()
-                                                        .enumerate()
-                                                        .map(|(i, f)| {
-                                                            let handle = ctx.load_texture(
+                                                    let frames = decoder
+                                                        .into_frames()
+                                                        .collect_frames()
+                                                        .expect("Can't decode frames");
+                                                    gif_images.images.insert(
+                                                        image.image_id.to_owned(),
+                                                        frames
+                                                            .iter()
+                                                            .enumerate()
+                                                            .map(|(i, f)| {
+                                                                let handle = ctx.load_texture(
                                                                 format!("gif_frame_{i}"),
                                                                 ColorImage::from_rgba_unmultiplied(
                                                                     [
@@ -222,56 +269,58 @@ pub fn ui_system(
                                                                 ),
                                                                 TextureOptions::default(),
                                                             );
-                                                            let (num, den) =
-                                                                f.delay().numer_denom_ms();
-                                                            (
-                                                                handle,
-                                                                (num as f32 * 1000.0 / den as f32)
-                                                                    .round()
-                                                                    as u32,
-                                                            )
+                                                                let (num, den) =
+                                                                    f.delay().numer_denom_ms();
+                                                                (
+                                                                    handle,
+                                                                    (num as f32 * 1000.0
+                                                                        / den as f32)
+                                                                        .round()
+                                                                        as u32,
+                                                                )
+                                                            })
+                                                            .collect(),
+                                                    );
+                                                }
+                                                let images = gif_images
+                                                    .images
+                                                    .get(&image.image_id.to_owned())
+                                                    .unwrap();
+                                                let frame = ((time.elapsed_seconds()
+                                                    / (images[0].1 as f32 / 500000.0))
+                                                    as usize)
+                                                    % images.len();
+                                                ui.add(
+                                                    egui::Image::new(&images[frame].0)
+                                                        .max_size(bevy_egui::egui::Vec2 {
+                                                            x: 400.0,
+                                                            y: 200.0,
                                                         })
-                                                        .collect(),
+                                                        .fit_to_exact_size(bevy_egui::egui::Vec2 {
+                                                            x: image.width,
+                                                            y: image.height,
+                                                        }),
+                                                );
+                                            } else {
+                                                ui.add(
+                                                    egui::Image::new(image.url.to_owned())
+                                                        .max_size(bevy_egui::egui::Vec2 {
+                                                            x: 400.0,
+                                                            y: 200.0,
+                                                        })
+                                                        .fit_to_exact_size(bevy_egui::egui::Vec2 {
+                                                            x: image.width,
+                                                            y: image.height,
+                                                        }),
                                                 );
                                             }
-                                            let images = gif_images
-                                                .images
-                                                .get(&image.image_id.to_owned())
-                                                .unwrap();
-                                            let frame = ((time.elapsed_seconds()
-                                                / (images[0].1 as f32 / 500000.0))
-                                                as usize)
-                                                % images.len();
-                                            ui.add(
-                                                egui::Image::new(&images[frame].0)
-                                                    .max_size(bevy_egui::egui::Vec2 {
-                                                        x: 400.0,
-                                                        y: 200.0,
-                                                    })
-                                                    .fit_to_exact_size(bevy_egui::egui::Vec2 {
-                                                        x: image.width,
-                                                        y: image.height,
-                                                    }),
-                                            );
-                                        } else {
-                                            ui.add(
-                                                egui::Image::new(image.url.to_owned())
-                                                    .max_size(bevy_egui::egui::Vec2 {
-                                                        x: 400.0,
-                                                        y: 200.0,
-                                                    })
-                                                    .fit_to_exact_size(bevy_egui::egui::Vec2 {
-                                                        x: image.width,
-                                                        y: image.height,
-                                                    }),
-                                            );
-                                        }
-                                    },
+                                        },
+                                    }
                                 }
-                            }
-                        });
-                    })
-                });
+                            });
+                        })
+                    });
+            });
         });
     });
 }
